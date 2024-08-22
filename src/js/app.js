d5ee5985a66055014d2b43e817292a41cd8730ed
Valentin <valentinvalentinov027@gmail.com>
2024-08-22 15:41:48 +0300

    Add download password functionality
    Add option for the user to download the password as a text file on their machine

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,36 @@
 import { fireErr, fireSuccess } from "./alertUtil.js";
 
-import {
-    passLengthErr,
-    copyPassSuccss,
-    emptyPassWhenCopyErr,
-    noCheckboxesCheckedErr,
-    specialSymbols
-} from './constants.js';
+import * as constants from './constants.js';
 
 const form = document.querySelector('.formSection form');
 const slider = document.getElementById('passLengthRange');
 const copyPassBtn = document.querySelector('.copyPasswordBtn');
 const passLengthSpan = document.getElementById('passLengthValue');
+const downloadPassBtn = document.querySelector('.downloadPasswordBtn');
 const generatedPasswordEl = document.getElementById('generatedPassword');
+
+downloadPassBtn.addEventListener('click', () => {
+    const password = generatedPasswordEl.textContent;
+
+    if (!password) {
+        fireErr(constants.emptyPassWhenDownloadErr);
+        return;
+    }
+
+    const blob = new Blob([password], { type: 'text/plain' });
+
+    const link = document.createElement('a');
+
+    link.download = 'password.txt';
+
+    link.href = window.URL.createObjectURL(blob);
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+});
 
 const checkboxContainer = document.querySelectorAll('.checkboxContainer');
 
@@ -30,15 +48,14 @@ copyPassBtn.addEventListener('click', () => {
     const password = generatedPasswordEl.textContent;
 
     if (!password) {
-        fireErr(emptyPassWhenCopyErr);
+        fireErr(constants.emptyPassWhenCopyErr);
         return;
     }
 
     navigator.clipboard.writeText(password)
-        .then(() => fireSuccess(copyPassSuccss))
+        .then(() => fireSuccess(constants.copyPassSuccss))
         .catch((reason) => fireErr(reason));
 });
-
 
 slider.addEventListener('input', (e) => passLengthSpan.textContent = e.currentTarget.value);
 
@@ -60,27 +77,35 @@ form.addEventListener('submit', (e) => {
 
     var formData = new FormData(form);
 
-    if (!formData.has('numbers') &&
-        !formData.has('special') &&
-        !formData.has('lowercase') &&
-        !formData.has('uppercase')) {
-            fireErr(noCheckboxesCheckedErr);
+    const includesNumbers = formData.has('numbers');
+    const includesSpecial = formData.has('special');
+    const includesLowercase = formData.has('lowercase');
+    const includesUppercase = formData.has('uppercase');
+
+    const noOptionsChecked =
+        !includesNumbers &&
+        !includesSpecial &&
+        !includesLowercase &&
+        !includesUppercase;
+
+    if (noOptionsChecked) {
+        fireErr(constants.noCheckboxesCheckedErr);
         return;
     }
 
     const generatedPassword = generatePassword(
-        formData.has('lowercase'),
-        formData.has('uppercase'),
-        formData.has('numbers'),
-        formData.has('special'),
+        includesLowercase,
+        includesUppercase,
+        includesNumbers,
+        includesSpecial,
         Number(formData.get('passLengthRange')));
 
     generatedPasswordEl.textContent = generatedPassword;
 
-    generatedPasswordEl.classList.add('animate__animated', 'animate__backInLeft', 'animate__faster');
+    generatedPasswordEl.classList.add('animate__animated', 'animate__zoomIn', 'animate__faster');
     generatedPasswordEl.addEventListener(
         'animationend',
-        (e) => e.currentTarget.classList.remove('animate__animated', 'animate__backInLeft', 'animate__faster'),
+        (e) => e.currentTarget.classList.remove('animate__animated', 'animate__zoomIn', 'animate__faster'),
         { once: true }
     );
 });
@@ -88,7 +113,7 @@ form.addEventListener('submit', (e) => {
 const getRandomLower = () => String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 const getRandomUpper = () => String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 const getRandomNumber = () => Number(String.fromCharCode(Math.floor(Math.random() * 10) + 48));
-const getRandomSymbol = () => specialSymbols[Math.floor(Math.random() * specialSymbols.length)];
+const getRandomSymbol = () => constants.specialSymbols[Math.floor(Math.random() * constants.specialSymbols.length)];
 
 const randomFunc = {
     lower: getRandomLower,
@@ -99,12 +124,16 @@ const randomFunc = {
 
 function generatePassword(lower, upper, number, symbol, length) {
     if (length < 10 || length > 100) {
-        fireErr(passLengthErr);
+        fireErr(constants.passLengthErr);
     }
 
     let generatedPassword = '';
     const typesCount = lower + upper + number + symbol;
-    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+    const typesArr = [
+        { lower },
+        { upper },
+        { number },
+        { symbol }].filter(item => Object.values(item)[0]);
 
     for (let i = 0; i < length; i += typesCount) {
         typesArr.forEach(type => {
--- src/js/constants.js
+++ src/js/constants.js
@@ -4,3 +4,4 @@ export const copyPassSuccss = 'Copied to Clipboard!';
 export const passLengthErr = 'The password length must be between 10 and 100 characters long!';
 export const emptyPassWhenCopyErr = 'You must first generate a password to copy it! I know right!';
 export const noCheckboxesCheckedErr = 'You must check at least one of the options listed in the form!';
+export const emptyPassWhenDownloadErr = 'To download the password as .txt file you must generate one! Bummer, right!';
